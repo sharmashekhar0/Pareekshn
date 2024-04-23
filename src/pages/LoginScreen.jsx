af455f7ad6cc03de2027c01bccb877c3d64bf3df
weaversinfotech <dev.weaversinfotech@gmail.com>
2024-04-23 17:44:12 +0530

    add changes to login screen

--- src/pages/LoginScreen.jsx
+++ src/pages/LoginScreen.jsx
@@ -24,31 +24,32 @@ function LoginScreen() {
 					/>
 				</div>
 				<div className="bg-[#ffffff] h-4/5 w-3/4 lg:w-4/5 xl:w-2/3 rounded-3xl p-6 flex flex-col gap-4 relative z-10">
-					<h1 className="text-xl font-bold text-[#a6a6a6]">
-						Candidate Login
+					<h1 className="text-xl font-medium text-[#858585]">
+						Candidate <br />
+						Login
 					</h1>
 					<div className="bg-[#1b4581] text-white h-12 p-4 text-center rounded-3xl flex items-center justify-center gap-4 hover:cursor-pointer">
 						<BsThreeDots className="text-[#1b4581] bg-white rounded h-5 w-8" />
-						<p className="text-sm">Login with OTP</p>
+						<p className="text-sm font-medium">Login with OTP</p>
 					</div>
 					<div className="relative w-full mx-auto my-2">
-						<div className="absolute top-1/2 left-0 w-full border-t border-gray-400"></div>
-						<div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-6 h-6 bg-[#cecece] rounded-full flex items-center justify-center">
-							<span className="text-[#1b4581] text-[12px] font-bold">
+						<div className="absolute top-1/2 left-0 w-full border-t border-[#9E9E9E73]"></div>
+						<div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-6 h-6 bg-[#CECECE] rounded-full flex items-center justify-center">
+							<span className="text-[#5C5C5C] text-[12px] font-medium">
 								OR
 							</span>
 						</div>
 					</div>
-					<p className="text-center text-sm font-bold text-[#7c7b7b]">
+					<p className="text-center text-sm font-medium text-[#383838]">
 						Login With Username/Email
 					</p>
 					<div className="flex flex-col gap-2">
 						<div className="relative h-10">
-							<IoPerson className="absolute top-1/2 left-2 transform -translate-y-1/2 text-[#7c7b7b]" />
+							<IoPerson className="absolute top-1/2 left-2 transform -translate-y-1/2 text-[#1C4481]" />
 							<input
 								type="text"
 								placeholder="Username/Email"
-								className="pl-8 pr-4 w-full h-10 shadow-customShadow outline-none placeholder:text-sm placeholder-[#7c7b7b]"
+								className="pl-8 pr-4 w-full h-10 shadow-customShadow outline-none placeholder:text-sm placeholder:text-[#1C4481]"
 							/>
 						</div>
 						<p className="text-[#7c7b7b] text-sm hover:cursor-pointer">
@@ -57,26 +58,26 @@ function LoginScreen() {
 					</div>
 					<div className="flex flex-col gap-2">
 						<div className="relative h-10">
-							<LuSquareAsterisk className="absolute top-1/2 left-2 transform -translate-y-1/2 text-[#7c7b7b]" />
-							<VscEye className="absolute top-1/2 right-2 transform -translate-y-1/2 text-[#7c7b7b]" />
+							<LuSquareAsterisk className="absolute top-1/2 left-2 transform -translate-y-1/2 text-[#1C4481]" />
+							<VscEye className="absolute top-1/2 right-2 transform -translate-y-1/2 text-[#1C4481]" />
 							<FaCircleExclamation className="absolute top-1/2 right-[-20px] transform -translate-y-1/2 text-[#1b4581]" />
 							<input
 								type="password"
 								placeholder="Password"
-								className="w-full h-10 p-4 pl-8 shadow-customShadow outline-none text-sm placeholder:text-sm placeholder:text-[#7c7b7b]"
+								className="w-full h-10 p-4 pl-8 shadow-customShadow outline-none text-sm placeholder:text-sm placeholder:text-[#1C4481]"
 							/>
 						</div>
-						<p className="text-[#7c7b7b] text-sm hover:cursor-pointer">
+						<p className="text-[#389393] text-sm hover:cursor-pointer">
 							Forget Password?
 						</p>
 					</div>
 					<div className="bg-[#1b4581] text-white h-12 p-2 flex justify-center items-center rounded-3xl hover:cursor-pointer">
 						<p>Login</p>
 					</div>
-					<div className="text-sm text-center font-semibold text-[#7c7b7b]">
+					<div className="text-sm text-center font-semibold text-[#3C4345]">
 						<p>
 							Not registered yet?{" "}
-							<span className="text-[#67a6b5] hover:cursor-pointer">
+							<span className="text-[#0F9FCC] hover:cursor-pointer font-medium">
 								Create an account
 							</span>
 						</p>
@@ -87,14 +88,16 @@ function LoginScreen() {
 			{/* Right Side */}
 			<div className="w-full lg:w-1/2 flex items-center justify-center p-8 sm:p-0">
 				<div className="w-5/6 lg:w-4/5 gap-8 xl:w-2/3 bg-[#e4ecf9] p-8 rounded-3xl flex flex-col items-center justify-between py-14">
-					<p className="text-center text-[#1b4581]">
+					<p className="text-center text-[#1C4481] font-medium">
 						Lorem ipsum dolor sit, amet consectetur adipisicing.
 					</p>
 					<div className="bg-[#1b4581] flex items-center justify-center gap-2 text-sm px-4 sm:px-10 py-3 rounded-3xl text-white hover:cursor-pointer">
 						<BsThreeDots className="text-white p-1 rounded-full  border-white border-2 h-8 w-8" />
-						<p className="text-nowrap">Login Via Exam Passcode</p>
+						<p className="text-nowrap font-medium">
+							Login Via Exam Passcode
+						</p>
 					</div>
-					<p className="text-[#1b4581] text-sm">
+					<p className="text-[#494949] text-sm font-medium">
 						Lorem ipsum dolor sit.
 					</p>
 				</div>
